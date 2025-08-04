5dda0d38eaa6c0e42dfd7ab4ef9e8de088320f04
David Rambo <davidrambo@mailfence.com>
2025-08-03 20:42:48 -0800

    tests for init command

---
+++ tests/init.rs
@@ -0,0 +1,77 @@
+// Tests the init command.
+use assert_cmd::prelude::*; // Add methods on commands
+use assert_fs::prelude::*; // Add methods on commands
+use predicates::prelude::*; // For writing assertions
+use std::{error::Error, process::Command}; // Run programs
+
+#[test]
+fn init_new_repo_in_cd() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    let tmpdir = assert_fs::TempDir::new()?;
+    std::env::set_current_dir(tmpdir.path())?;
+
+    cmd.assert().success().stdout(predicate::str::contains(
+        "Initialized empty Gitlet repository",
+    ));
+
+    tmpdir.child(".gitlet").assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/blobs")
+        .assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/commits")
+        .assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/refs")
+        .assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/index")
+        .assert(predicate::path::is_dir());
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn init_new_repo_in_path() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    let tmpdir = assert_fs::TempDir::new()?;
+
+    cmd.arg("init").arg(tmpdir.path());
+    cmd.assert().success().stdout(predicate::str::contains(
+        "Initialized empty Gitlet repository",
+    ));
+
+    tmpdir.child(".gitlet").assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/blobs")
+        .assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/commits")
+        .assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/refs")
+        .assert(predicate::path::is_dir());
+    tmpdir
+        .child(".gitlet/index")
+        .assert(predicate::path::is_dir());
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn init_fails_repo_already_exists() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    let tmpdir = assert_fs::TempDir::new()?;
+    let tdpath = tmpdir.path();
+    std::fs::create_dir(tdpath.join(".gitlet"))
+        .expect("Failed to create .gitlet directory in TempDir");
+
+    cmd.arg("init").arg(tdpath);
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Gitlet repository already exists"));
+
+    Ok(())
+}
