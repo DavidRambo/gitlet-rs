04db76fea645b5980247bddbcfc208b2658a49bd
David Rambo <davidrambo@mailfence.com>
2025-08-08 15:10:11 -0800

    doc: docstring for test utility function

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -5,6 +5,7 @@ use anyhow::Result;
 
 static DIRLOCK: LazyLock<Mutex<u8>> = LazyLock::new(|| Mutex::new(0));
 
+/// Helper function for multithreaded unit tests to safely set the current working directory.
 pub fn set_dir<F>(dir: &std::path::Path, clos: F) -> Result<()>
 where
     F: Fn() -> Result<()>,
