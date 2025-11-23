a43b9c02cee06e82535c0c6285ebd202da0c609c
David Rambo <davidrambo@mailfence.com>
2025-11-23 12:14:30 -0800

    test: write more merge tests

--- tests/merge.rs
+++ tests/merge.rs
@@ -1,9 +1,13 @@
 //! Tests the merge command.
 
 use assert_cmd::Command;
-use assert_fs::TempDir;
+use assert_fs::{
+    TempDir,
+    assert::PathAssert,
+    prelude::{FileWriteStr, PathChild},
+};
 use predicates::prelude::predicate;
-use std::error::Error;
+use std::{error::Error, io::Read};
 
 /// Setup function for merge tests.
 ///
@@ -53,6 +57,83 @@ fn setup_merge_tests() -> Result<TempDir, Box<dyn Error>> {
     Ok(tmpdir)
 }
 
+/// Attempts to merge with unstaged changes made to the a.txt file in main.
+#[test]
+fn merge_unstaged_changes() -> Result<(), Box<dyn Error>> {
+    let tmpdir = setup_merge_tests()?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("switch").arg("main").unwrap();
+
+    let mut cmd = Command::new("echo");
+    cmd.current_dir(&tmpdir)
+        .arg("Some text")
+        .arg(">>")
+        .arg("a.txt")
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("merge").arg("dev");
+    cmd.assert().failure().stdout(predicate::str::contains(
+        "There is a file with unstaged changes.",
+    ));
+
+    Ok(())
+}
+
+/// Attempts to merge with staged, but uncommitted, changes made to the a.txt file in main.
+#[test]
+fn merge_uncommitted_changes() -> Result<(), Box<dyn Error>> {
+    let tmpdir = setup_merge_tests()?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("switch").arg("main").unwrap();
+
+    let mut cmd = Command::new("echo");
+    cmd.current_dir(&tmpdir)
+        .arg("Some text")
+        .arg(">>")
+        .arg("a.txt")
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("add").arg("a.txt");
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("merge").arg("dev");
+    cmd.assert()
+        .failure()
+        .stdout(predicate::str::contains("You have uncommited changes."));
+
+    Ok(())
+}
+
+#[test]
+fn merge_nonexistent_branch() -> Result<(), Box<dyn Error>> {
+    let tmpdir = setup_merge_tests()?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("merge").arg("not_here");
+    cmd.assert()
+        .failure()
+        .stdout(predicate::str::contains("That branch does not exist."));
+
+    Ok(())
+}
+
+#[test]
+fn merge_self() -> Result<(), Box<dyn Error>> {
+    let tmpdir = setup_merge_tests()?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("merge").arg("dev");
+    cmd.assert().failure().stdout(predicate::str::contains(
+        "Cannot merge a branch with itself.",
+    ));
+
+    Ok(())
+}
+
 #[test]
 fn merge_new_file() -> Result<(), Box<dyn Error>> {
     let tmpdir = setup_merge_tests()?;
@@ -67,4 +148,100 @@ fn merge_new_file() -> Result<(), Box<dyn Error>> {
         .stdout(predicate::str::contains("Merged dev into main"));
 
     Ok(())
+}
+
+#[test]
+fn merge_file_change() -> Result<(), Box<dyn Error>> {
+    let tmpdir = setup_merge_tests()?;
+
+    let atxt_file = tmpdir.child("a.txt");
+    atxt_file.write_str("Some new text")?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("add").arg("a.txt");
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir)
+        .arg("commit")
+        .arg("Added text to a.txt");
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("switch").arg("main").unwrap();
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("merge").arg("dev");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Merged dev into main"));
+
+    atxt_file.assert(predicate::str::contains("Some text"));
+
+    Ok(())
+}
+
+/// Merges a file with a conflict.
+///
+/// First adds "Dev text" to a.txt in the dev branch.
+/// Then checks out main and modifies a.txt to contain 'Main text'.
+/// Adds and commits the change, then merges with dev branch.
+/// a.txt should contain:
+///     <<<<<<< HEAD
+///     Main text
+///     =======
+///     Dev text
+///     >>>>>>> {head_dev_commit_id}
+#[test]
+fn merge_file_conflict() -> Result<(), Box<dyn Error>> {
+    let tmpdir = setup_merge_tests()?;
+
+    let atxt_file = tmpdir.child("a.txt");
+    atxt_file.write_str("Dev text")?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("add").arg("a.txt");
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir)
+        .arg("commit")
+        .arg("Wrote 'Dev text' to a.txt");
+
+    // Save the commit id, which is now stored in HEAD, before switching to main.
+    let head_file = tmpdir.child(".gitlet/HEAD");
+    let mut head_file = std::fs::File::open(head_file)?;
+    let mut head_dev_commit_id = String::with_capacity(41);
+    let _ = head_file.read_to_string(&mut head_dev_commit_id)?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("switch").arg("main").unwrap();
+
+    let atxt_file = tmpdir.child("a.txt");
+    atxt_file.write_str("Main text")?;
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("add").arg("a.txt");
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir)
+        .arg("commit")
+        .arg("Wrote 'Main text' to a.txt");
+
+    let mut cmd = Command::cargo_bin("gitlet")?;
+    cmd.current_dir(&tmpdir).arg("merge").arg("dev");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Encountered a merge conflict."));
+
+    let expected = "<<<<<<< HEAD\nHead text\n=======\nDev text\n>>>>>>> {head_dev_commit_id}\n";
+    atxt_file.assert(predicate::str::contains(expected));
+
+    Ok(())
+}
+
+/// Ensures that an attempted merge between branches with disparate commit histories fails.
+/// For a repo to end up in this bad state, a branch would need to be manually created
+/// in the .gitlet directory or an existing branch's commit history would need to be
+/// manually tampered with.
+#[test]
+fn merge_no_split() -> Result<(), Box<dyn Error>> {
+    todo!()
 }
